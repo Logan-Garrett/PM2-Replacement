e19913ea98e8436a02e77e0a056c8ad58dfc284a
NineTailSecurity <ninetailsoftware@gmail.com>
2023-12-25 00:42:01 -0500

    Maybe some progress

--- Task-Master.c
+++ Task-Master.c
@@ -8,5 +8,26 @@ int main(int argc, char *argv[]) {
 	char *action = argv[1];
 	printf("Action being performed: %s\n", action);
 
+	char *program = argv[2];
+	printf("Program being run: %s\n", program);
+
+
+	// Issue maybe arises in which program un-compiled. Work on maybe?
+	if (strcmp(argv[1], "start") != 1) {
+		int errorCheck;
+		char command[100];
+
+		if (strstr(argv[2], "./") != NULL) {
+			errorCheck = system(program);
+		} else {
+			sprintf(command, "./%s", program);
+			errorCheck = system(command);
+		}
+
+		if (errorCheck == 1) {
+			printf("Error Running Program\n");
+		}
+	}
+
 	return 0;
 }
